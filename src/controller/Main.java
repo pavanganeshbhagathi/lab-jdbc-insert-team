53166442edac1ef358946fcb3d30f7d85e97e6c3
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-28 09:34:02 +0530

    last

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,59 @@
 
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+import dao.CityDAO;
+import dao.TeamDAO;
+import model.City;
+import model.Team;
+
+public class Main 
+// Did you forget the main method??
+// Without String args how your java application will work??
+//yes sir,sorry.i fprget to define
+// When you create a class itself you can define automatically
+{
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		CityDAO citydao=new CityDAO();
+		TeamDAO teamdao=new TeamDAO();
+		
+		System.out.println("Enter city name");
+		String cityName=br.readLine();
+		
+		System.out.println("Enter city id");
+		long id=Long.parseLong(br.readLine());
+		
+		City city=new City(id,cityName);
+		
+		System.out.println("Enter your  name");
+		String name=br.readLine();
+		
+		System.out.println("Enter coach name");
+		String coachName=br.readLine();
+		
+		System.out.println("Enter captian name");
+		String captianName=br.readLine();
+		
+		
+		Team team=new Team(name,coachName,captianName,city);
+		
+		teamdao.createTeam(team);
+		
+		System.out.println(" Enter team name to get deatils of cityname and cityID");
+		
+		String check=br.readLine();
+	city=	citydao.getCityByName(check);
+	
+	System.out.println("City id = "+city.getCityId());
+	System.out.println("city name = "+city.getCityName());
+		
+	}
+	
+}
